3de6a93daa6cccdc567b8778a483054d58ea8451
HackHut <acidm4n@protonmail.com>
2018-04-11 20:41:21 +0100

    Update GetBitcoinBlock.py

--- GetBitcoinBlock.py
+++ GetBitcoinBlock.py
@@ -1,50 +1,46 @@
-with open('old.txt', 'r') as f:
-    lines = f.readlines()
-    previous_block = '507101'
-    num_of_ones = 0
-    num_of_zeros = 0
-    total_line = 0
-    results = []
-    loop = 5
-    for line in lines:
-        line.strip('\n')
-        if previous_block == line:
-            num_of_zeros += 1
-            results.append('0')
-        else:
-            num_of_ones += 1
-            results.append('1')
-        previous_block=line
-        total_line += 1
+import requests, json, time
 
-    print("There are " + str(total_line) + " lines")
-    print("Estimated number of block for 10 min interval: " + str(total_line/10))
-    print("Actual number of blocks: " + str(num_of_ones))
+'''
+This program calculates the current Bitcoin block every minute (plus the time taken to make the request) over a 12 hour period.
+'''
 
-    cumulative = 0
-    cum = []
-    for x in range(0, len(results)):
-        if results[x] == '0':
-            pass
-            #print(cumulative)
-        elif results[x] == '1':
-            cumulative += 1
-            # print(cumulative)
-        else:
-            print('error')
+def getBlock():
+    results = list() ##where the results are stored
+    for minutes in range(0, 720): ##queryies the bitcoin network ever minute for 12 hours to see if a new block has been propegated
+        try:
+            current_blockcount = requests.get("https://blockexplorer.com/api/status?q=getBlockCount")
+            # gets current blockcount for bitcoin network
+            block_json = json.loads(current_blockcount.text)
+            # converts to JSON
+        except ValueError:
+            print("Json value error")
+        current_block = str(block_json['blockcount'])
+        print("Current block:" + current_block + " Current time:" + str(minutes))
+        time.sleep(60)
+        if minutes == 0:
+            starting_block = str(block_json['blockcount'])
 
-        cum.append(cumulative)
+        if current_block == previous_block:
+            results.append(str(0))
+        if current_block == (int(previous_block) + 1):
+            results.append(str(1))
+        if current_block == (int(previous_block) + 2):
+            results.append(str(2))
 
-    n = 0
-    temp = 0
-    number_of_blocks_in_10_mins = [0,0,0,0,0]
-    for y in range(0, len(cum)):
-        n = n + 1
-        temp = temp + (cum[y] - cum[y-1])
-        #print(temp)
-        if n % 10 == 0:
-            print(temp)
-            if temp >= 0:
-                number_of_blocks_in_10_mins[temp] = number_of_blocks_in_10_mins[temp] + 1
-            temp = 0
-    print(number_of_blocks_in_10_mins)
+        previous_block = current_block
+
+    fin_block = str(block_json['blockcount'])
+    block_elasped = int(fin_block) - int(starting_block)
+
+    # output results
+    print("starting_block: " + str(starting_block))
+    print("finishing_block: " + str(fin_block))
+    print("Time_elaspased: 12 hours/720 mins")
+    print("blocks_elasped: " + str(block_elasped))
+    print("expected_number_of_blocks: 72")
+
+    print(*results, sep=",")
+
+
+getBlock()
+
